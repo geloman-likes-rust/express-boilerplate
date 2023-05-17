47786f7a08b98cf692028d35787563e4241de864
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-18 01:40:32 +0800

    Update entry point: index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"
+import rootRoute from "./routes/index.js"
+
 let app = express()
 let PORT = process.env.SERVER_PORT || 6969
 
-app.get("/", (_, res) => res.status(200).json({ sucess: true, data: { greetings: "hello!" } }))
+app.use("/", rootRoute)
 
 let handleServerConnection = (error) => console.log(error ? `Error message: ${error}` : `Server is Listening on Port ${PORT}`)
 app.listen(PORT, handleServerConnection)
