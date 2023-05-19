12f1f36fa402b3ab3d2ae7afac808b992e28108d
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-20 02:46:00 +0800

    Update entry point

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import rootRoute from "./routes/index.js"
-import corsValidation from "./middlewares/corsValidation.js"
+// import corsValidation from "./middlewares/corsValidation.js"
 
 let app = express()
 let PORT = process.env.SERVER_PORT || 6969
