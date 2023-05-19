76d61d922c89d095292f4539e4d3f481ec87207f
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-20 02:32:24 +0800

    Update entry point

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import corsValidation from "./middlewares/corsValidation.js"
 let app = express()
 let PORT = process.env.SERVER_PORT || 6969
 
-app.options(corsValidation)
+app.use(corsValidation)
 app.use("/", rootRoute)
 
 let handleServerConnection = (error) => console.log(error ? `Error message: ${error.message}` : `Server is Listening on Port ${PORT}`)
