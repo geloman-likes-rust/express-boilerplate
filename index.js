8b8aac087db9c956a7f861f5e6471f59f50bcd9f
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-20 02:43:03 +0800

    Update entry point

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import corsValidation from "./middlewares/corsValidation.js"
 let app = express()
 let PORT = process.env.SERVER_PORT || 6969
 
-app.use(corsValidation)
+// UNCOMMENT THIS LATER (this message is for gelo)
+// app.use(corsValidation)
 app.use("/", rootRoute)
 
 let handleServerConnection = (error) => console.log(error ? `Error message: ${error.message}` : `Server is Listening on Port ${PORT}`)
