59e0d761ed73d9043f5e6a5a3817587461e1ec5b
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-20 01:13:37 +0800

    Create config folder

---
+++ config/mysql.js
@@ -0,0 +1,8 @@
+const mysqlConfig = {
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DB,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD
+}
+
+export default mysqlConfig
