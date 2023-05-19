0453ec5b74748bf74e141cb2fb3e09ce49e5384b
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-20 01:13:18 +0800

    Remove db-config

--- db-config/index.js
+++
@@ -1,8 +0,0 @@
-const mysqlConfig = {
-  host: process.env.MYSQL_HOST,
-  database: process.env.MYSQL_DB,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD
-}
-
-export { mysqlConfig }
