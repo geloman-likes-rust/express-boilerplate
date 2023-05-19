350e7c5453ef19d491a56d4149b46a1c15484eda
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-20 02:53:34 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-## npm init -y
+# GET THE CURRENT DIRECTORY NAME
+current_directory_name=$(basename "$PWD")
+
+# UPDATE the "name" field in package.json
+sed -i "s/\"name\": \".*\"/\"name\": \"$current_directory_name\"/" package.json
+
+# INSTALLATION & SETUP
 npm i express dotenv cors
 npm i nodemon --save-dev
 rm -rdf .git
