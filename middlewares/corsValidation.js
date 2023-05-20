ae11f862de19aea98c7dde33276838b62335c17b
geloman <geloman-likes-rust@users.noreply.github.com>
2023-05-20 17:47:33 +0800

    Update cors validation middleware

--- middlewares/corsValidation.js
+++ middlewares/corsValidation.js
@@ -5,7 +5,7 @@ dotenv.config()
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.replace(" ", "").split(",")
 
 const OPTIONS = {
-  origin: ALLOWED_ORIGINS || ["http://localhost"],
+  origin: ALLOWED_ORIGINS,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }
